0f7835816eaf952d25281ade6baf6c5f1507cd06
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-11-28 03:43:25 +0100

    Added home page Animation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,10 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
