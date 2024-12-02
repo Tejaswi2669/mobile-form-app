df6a235551b5802e88bf278c8ff50df5627c70f5
Tejaswi2669 <tejaswinerella9@gmail.com>
2024-12-02 13:25:06 +0100

    Auto-generated commit

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:1337/api', // Base URL for Strapi APIs
-};
--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// src/environments/environment.ts
-
-export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.1.54:1337/api', // Base URL for Strapi APIs
-};
